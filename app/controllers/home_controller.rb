1aa2d7343c8daece49995a16dd2086b0aa67bc00
basil <basil.mawejje@gmail.com>
2018-08-30 21:47:11 +0300

    Add fun facts section to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,11 @@ class HomeController < ApplicationController
     @donors = Donor.all.limit(4)
     @latest_news = LatestNews.all.limit(1)
     @countries = CS.get
+
+    # Added models without limit for facts counter
+    @donor = Donor.all
+    @work = Work.all
+    @latest_news = LatestNews.all
+    @countries = CS.get
   end
 end
