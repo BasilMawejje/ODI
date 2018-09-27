e88817f507f27b3625a5eb35f00c50ee19b65a4b
basil <basil.mawejje@gmail.com>
2018-09-27 12:55:59 +0300

    Done basic data entry Fix bug on facts counter Change favicon Add youtube social link Add phone number

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,5 @@ class HomeController < ApplicationController
     @work = Work.all.limit(4)
     @donors = Donor.all.limit(4)
     @latest_news = LatestNews.all.limit(1)
-
-    # Added models without limit for facts counter
-    @donor = Donor.all
-    @work = Work.all
-    @latest_news = LatestNews.all
   end
 end
