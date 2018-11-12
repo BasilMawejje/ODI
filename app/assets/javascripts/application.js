e1590f2de218ad70ee5ea2c2149c369ea0145fa1
BasilMawejje <basil.mawejje@gmail.com>
2018-11-12 14:40:12 +0300

    Add Js counter to counter section facts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,3 +24,28 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+<script type="text/javascript">
+  $(window).scroll(function() {
+    var hT = $('#firstCountItem').offset().top,
+        hH = $('#firstCountItem').outerHeight(),
+        wH = $(window).height(),
+        wS = $(this).scrollTop();
+    console.log((hT - wH), wS);
+    if (wS > (hT + hH - wH)) {
+        $('.count').each(function() {
+            $(this).prop('Counter', 0).animate({
+                Counter: $(this).text()
+            }, {
+                duration: 900,
+                easing: 'swing',
+                step: function(now) {
+                    $(this).text(Math.ceil(now));
+                }
+            });
+        }); {
+            $('.count').removeClass('count').addClass('counted');
+        };
+    }
+});
+</script>
