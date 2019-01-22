4dc8e2ce6d71149020229d4b9d69992eac42274d
BasilMawejje <basil.mawejje@gmail.com>
2019-01-22 11:23:29 +0300

    Fix work grid on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @work = Work.all.limit(4)
+    @work = Work.all.limit(3)
     @donors = Donor.all.limit(4)
     @latest_news = LatestNews.all.limit(1)
     @partners = Partner.all
